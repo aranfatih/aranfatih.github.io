575888e363b6033b9fad95932fd9b10d5d2e2d53
aran <othman.aran@gmail.com>
2019-12-21 22:41:07 +0300

    initial commit

---
+++ precache-manifest.4a3bb991f01eebf7ca7909c097635e39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d85726f2cf59f0287ade058bf8637d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1a513b495105f958531",
+    "url": "/static/css/2.374fe853.chunk.css"
+  },
+  {
+    "revision": "49317b92fe892f6f7ea9",
+    "url": "/static/css/main.b758ec4b.chunk.css"
+  },
+  {
+    "revision": "f1a513b495105f958531",
+    "url": "/static/js/2.e9132278.chunk.js"
+  },
+  {
+    "revision": "49317b92fe892f6f7ea9",
+    "url": "/static/js/main.a010c34f.chunk.js"
+  },
+  {
+    "revision": "9ac418c87327fc1c2f3b",
+    "url": "/static/js/runtime-main.e31ca81b.js"
+  },
+  {
+    "revision": "0b2c0e74f616b916c9ca2027f3ddc2f3",
+    "url": "/static/media/DroidKufi-Regular.0b2c0e74.woff"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/static/media/Roboto-Medium.58aef543.ttf"
+  }
+]);
